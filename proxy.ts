aa41433543a549a862e8e99b5c15fe13f35dc51d
Kent0710 <christiankent001@gmail.com>
2025-11-01 02:06:21 +0800

    fix: remove validating session in proxy

--- proxy.ts
+++ proxy.ts
@@ -1,27 +1,18 @@
-export const runtime = "nodejs";
-
+// middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getAuth } from "firebase-admin/auth";
 
-export async function proxy(req: NextRequest) {
+export function middleware(req: NextRequest) {
     const sessionCookie = req.cookies.get("session")?.value;
 
-    // if no session cookie, redirect to login
     if (!sessionCookie) {
         return NextResponse.redirect(new URL("/signIn", req.url));
     }
 
-    try {
-        await getAuth().verifySessionCookie(sessionCookie, true);
-        return NextResponse.next(); // allow access
-    } catch (error) {
-        console.error("Invalid or expired session:", error);
-        return NextResponse.redirect(new URL("/signIn   ", req.url));
-    }
+    // No firebase-admin here!
+    return NextResponse.next();
 }
 
-// Apply to protected routes only
 export const config = {
-    matcher: ["/home"], // protect everything under /dashboard and /admin
+    matcher: ["/home/:path*"], // protect /home and subpaths
 };
