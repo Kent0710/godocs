54fb68f5d4a457901f57e56d8de0bca958ab9ee9
Kent0710 <christiankent001@gmail.com>
2025-11-01 09:57:56 +0800

    feat: add copy on click workspace code block

--- components/workspace/workspace-code-block.tsx
+++ components/workspace/workspace-code-block.tsx
@@ -1,20 +1,53 @@
+"use client";
+
 import { getWorkspaceCode } from "@/actions/workspace/get-workspaces";
 import { Button } from "../ui/button";
-import { UserPlus } from "lucide-react";
-
-// TODO: add copy functionality
+import { Copy} from "lucide-react";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 interface WorkspaceCodeBlockProps {
-    workspaceId : string;
+    workspaceId: string;
 }
 
-export default async function WorkspaceCodeBlock({workspaceId}: WorkspaceCodeBlockProps) {
-    const code = await getWorkspaceCode(workspaceId);
+export default function WorkspaceCodeBlock({
+    workspaceId,
+}: WorkspaceCodeBlockProps) {
+    const [ready, setIsReady] = useState(false);
+    const [code, setCode] = useState("");
+
+    useEffect(() => {
+        async function handleGetWorkspaceCode() {
+            if (!workspaceId) return;
+
+            const code = await getWorkspaceCode(workspaceId);
+            setCode(code);
+            setIsReady(true);
+        }
+
+        handleGetWorkspaceCode();
+
+        return () => {
+            setCode("");
+        };
+    }, [workspaceId]);
+
+    // copy when clicked
+    const handleClick = () => {
+        navigator.clipboard.writeText(code);
+        toast.success("Workspace code copied to clipboard!");
+    };
 
     return (
-        <Button variant={'outline'}>
-            <UserPlus />
-            {code}
+        <Button variant={"outline"} onClick={handleClick}>
+            {ready ? (
+                <>
+                    <Copy />
+                    <span>{code}</span>
+                </>
+            ) : (
+                <>Loading code...</>
+            )}
         </Button>
     );
 }
